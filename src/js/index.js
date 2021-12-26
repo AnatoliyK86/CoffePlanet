5f9f6df96d3f929b22b6391525e77362ab9852ab
Anatolii86 <kozachenkoav@gmail.com>
2021-12-26 23:30:24 +0200

    Сделан прелоадер

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+import WOW from 'wow.js';
+
+let wow = new WOW({
+	boxClass:     'animate',     
+    animateClass: 'animate__animated' 
+});
+wow.init();
+
+let preloader = document.querySelector('.preloader');
+preloader.addEventListener('animationend', function(e){
+	if( e.target === preloader ){
+		document.body.classList.remove('ov-h');
+	} 
+});
+
+// let objs = [
+// 	'.cup',
+// 	'.steam',
+// 	'.sleeve',
+// 	'.lid'
+// ];
+
+// objs.forEach( function(elem){
+// 	let pathLen = document.querySelector(elem).getTotalLength();
+// 	console.log(elem + ': ' + pathLen);
+// });
