aa130808556c08e78841c0de321ae224be4955ae
Anatolii86 <kozachenkoav@gmail.com>
2021-12-27 10:27:41 +0200

    Подключили слайдер

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import WOW from 'wow.js';
+import $ from 'jquery';
+import 'slick-carousel';
 
 let wow = new WOW({
 	boxClass:     'animate',     
@@ -6,12 +8,22 @@ let wow = new WOW({
 });
 wow.init();
 
+$('.article-slider__container').slick({
+	autoplay: true,
+	autoplaySpeed: 5000,
+	prevArrow: '.article-slider__btn_prev',
+	nextArrow: '.article-slider__btn_next'
+});
+
 let preloader = document.querySelector('.preloader');
 preloader.addEventListener('animationend', function(e){
 	if( e.target === preloader ){
 		document.body.classList.remove('ov-h');
 	} 
 });
+
+
+
 
 // let objs = [
 // 	'.cup',
